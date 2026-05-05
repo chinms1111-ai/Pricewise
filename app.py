9e5e64d33b91913a69e71abcf3f039fa8f4b5dc3
chinms1111-ai <chinms1111@gmail.com>
2026-05-05 06:21:04 -0700

    Add database initialization  for deployment

--- app.py
+++ app.py
@@ -6,10 +6,30 @@ from datetime import date
 app = Flask(__name__)
 CORS(app)
 
+
 def get_db():
     conn = sqlite3.connect("pricewise.db")
     conn.row_factory = sqlite3.Row
     return conn
+
+def init_db():
+    conn = get_db()
+    c = conn.cursor()
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS products (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT ,url TEXT)''')
+    c.execute('''CREATE TABLE IF NOT EXISTS price_history (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            product_id INTEGER,price REAL,date TEXT)''')
+    conn.commit()
+    conn.close()
+    
+init_db()
+    
+
+
+ 
 
 @app.route("/")
 def index():
